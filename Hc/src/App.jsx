c54858ecccd0b9ac4476b5171334849a6e519b1a
felipe <fdomingueskt@gmail.com>
2023-03-02 19:57:08 -0300

    feature: fazendo tabela, orçamento ou entrar em contato, estilizando um pouquinho melhor

--- Hc/src/App.jsx
+++ Hc/src/App.jsx
@@ -1,26 +1,48 @@
-import { Container, Div, Form, Input, Select, Label, ErrorYup, RenderP } from "./Styles"
-import { useForm } from "react-hook-form"
-import * as yup from "yup"
-import { yupResolver } from "@hookform/resolvers/yup"
-import { useState, useEffect } from "react"
-
+import {
+  Container,
+  Div,
+  Form,
+  Input,
+  Select,
+  Label,
+  ErrorYup,
+  RenderP,
+  Tabel,
+  TabelDad,
+  BtnSubmit
+} from "./Styles/index";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useState, useEffect } from "react";
 
 const schema = yup.object().shape({
   nome: yup.string().required("Campo obrigatório"),
   email: yup.string().email("E-mail inválido").required("Campo obrigatório"),
   telefone: yup.string().required("Campo obrigatório"),
-  area: yup.string().oneOf(["Desenvolvimento Web", "Desenvolvimento Mobile"], "Área inválida").required("Campo obrigatório"),
-  valor: yup.string().required("Campo obrigatório")
-})
-
+  area: yup
+    .string()
+    .oneOf(["Desenvolvimento Web", "Desenvolvimento Mobile"], "Área inválida")
+    .required("Campo obrigatório"),
+  valor: yup.string().required("Campo obrigatório"),
+});
 
 function App() {
   const [formData, setFormData] = useState(null);
   const [showResults, setShowResults] = useState(false);
+  const [showContactButton, setShowContactButton] = useState(false);
 
   function onSubmit(data) {
     setFormData(data);
     localStorage.setItem("formData", JSON.stringify(data));
+    const valor = parseInt(data.valor);
+    let showContactButton = false;
+    if (valor >= 4500 && valor <= 5000) {
+      showContactButton = true;
+    } else if (valor >= 8000 && valor <= 15000) {
+      showContactButton = true;
+    }
+    setShowContactButton(showContactButton);
     setShowResults(true);
   }
 
@@ -31,11 +53,17 @@ function App() {
     }
   }, []);
 
-  
+  const reset = () => {
+    localStorage.removeItem("formData");
+  };
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     resolver: yupResolver(schema),
-    defaultValues: JSON.parse(localStorage.getItem("formData")) || {}
+    defaultValues: JSON.parse(localStorage.getItem("formData")) || {},
   });
 
   return (
@@ -46,15 +74,18 @@ function App() {
             <Label>Nome completo:</Label>
             <Input {...register("nome")} defaultValue={formData?.nome} />
             {errors.nome && <ErrorYup>{errors.nome.message}</ErrorYup>}
-  
+
             <Label>E-mail:</Label>
             <Input {...register("email")} defaultValue={formData?.email} />
             {errors.email && <ErrorYup>{errors.email.message}</ErrorYup>}
-  
+
             <Label>Telefone:</Label>
-            <Input {...register("telefone")} defaultValue={formData?.telefone} />
+            <Input
+              {...register("telefone")}
+              defaultValue={formData?.telefone}
+            />
             {errors.telefone && <ErrorYup>{errors.telefone.message}</ErrorYup>}
-  
+
             <Select {...register("area")} defaultValue={formData?.area}>
               <option>Selecione</option>
               <option>Desenvolvimento Web</option>
@@ -64,18 +95,45 @@ function App() {
             <Label>Valor que deseja investir:</Label>
             <Input {...register("valor")} defaultValue={formData?.valor} />
             {errors.valor && <ErrorYup>{errors.valor.message}</ErrorYup>}
-  
-            <button type="submit">Enviar</button>
+
+            <BtnSubmit type="submit">Enviar</BtnSubmit>
+
+            {showResults && (
+              <>
+                <div>
+                  <RenderP>
+                    Olá {formData.nome}, aqui está seu orçamento {formData.area}{" "}
+                    no valor de {formData.valor}
+                  </RenderP>
+                  
+                  <button
+                    onClick={() => {
+                      setShowResults(false);
+                      reset();
+                    }}
+                  >
+                    Refazer orçamento
+                  </button>
+                </div>
+                <TabelDad>
+                  <Tabel>{formData.area}</Tabel>
+                  <Tabel>{formData.valor}</Tabel> 
+                  <Tabel>Desenvolvimento simples</Tabel>{" "}
+                  <Tabel>
+                    {" "}
+                    {showContactButton ? (
+                      <button style={{borderRadius: '8px'}}>Entre em contato</button>
+                    ) : (
+                      <button>Solicitar orçamento</button>
+                    )}
+                  </Tabel>
+                </TabelDad>
+              </>
+            )}
           </Form>
-          {showResults && (
-            <div>
-              <RenderP>Olá {formData.nome}, aqui está seu orçamento {formData.area} no valor de {formData.valor}</RenderP>
-              <button onClick={() => setShowResults(false)}>Refazer orçamento</button>
-            </div>
-          )}
         </Div>
       </Container>
     </>
-  )
+  );
 }
-export default App
+export default App;
--- Hc/src/styles/index.ts
+++ Hc/src/styles/index.ts
@@ -5,6 +5,9 @@ background-color: black;
 width: 100vw;
 height: 100vh;
 margin: -8px;
+display: flex;
+    align-items: center;
+    justify-content: space-around;
 `
 
 
@@ -18,16 +21,20 @@ flex-direction: column;
 export const Form = styled.form`
 display: flex;
 flex-direction: column;
-
+gap: 10px;
 `
 
 export const Input = styled.input`
-border: 1px solid red;
+border: 1px solid white;
+height: 40px;
+border-radius: 40px;
 
 `
 
 export const Select = styled.select `
 border: 1px solid red;
+height: 40px;
+border-radius: 40px;
 `
 
 export const Label = styled.label`
@@ -41,4 +48,26 @@ color: white;
 
 export const RenderP = styled.p `
 color: white;
+`
+
+
+export const Tabel = styled.div`
+border: 1px solid white;
+    width: 111px;
+    color: #0a0a0a;
+    background: white;
+    border-radius: 8px;
+`
+
+export const TabelDad = styled.div`
+display: flex;
+flex-direction: row;
+gap: 20px;
+`
+
+
+export const BtnSubmit = styled.button`
+height: 40px;
+border-radius: 40px;
+
 `
